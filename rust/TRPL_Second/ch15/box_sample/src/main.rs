c87d2ff5546db2055cb9c099df90dac399d77585
Shuhei Yamashita <b.e7472@gmail.com>
2017-12-27 17:51:05 +0900

    Rust: TRPL 15-1 [Fix] / 15-2 [WIP]

--- rust/TRPL_Second/ch15/box_sample/src/main.rs
+++ rust/TRPL_Second/ch15/box_sample/src/main.rs
@@ -1,9 +1,18 @@
 enum List {
-    Cons(i32, List),
+    // is error, recursive type.
+    // Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
+use List::{ Cons, Nil };
+
 fn main() {
-    let b = Box::new(5);
-    println!("b = {}", b);
+    // is error
+    // let list = Cons(1, Cons(2, Cons(3, Nil)));
+    
+    let list = Cons(1,
+        Box::new(Cons(2,
+            Box::new(Cons(3,
+                Box::new(Nil))))));
 }

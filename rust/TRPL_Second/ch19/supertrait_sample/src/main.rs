13a61764f5b88c26180b4b3b73d25ec3b448feef
Shuhei Yamashita <b.e7472@gmail.com>
2018-02-26 17:16:34 +0900

    TRPL 19-3 [Fix]

---
+++ rust/TRPL_Second/ch19/supertrait_sample/src/main.rs
@@ -0,0 +1,45 @@
+use std::fmt;
+
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}, {}", self.x, self.y)
+    }
+}
+
+impl OutlinePrint for Point {}
+
+// Newtype Pattern
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(",. "))
+    }
+}
+
+fn main() {
+    let point = Point { x: 4, y: 5 };
+    point.outline_print();
+
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
+}
+
+

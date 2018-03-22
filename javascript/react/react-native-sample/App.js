8b7b4fb1a2c6df1a65c37938066b7bc71b98675e
Shuhei Yamashita <b.e7472@gmail.com>
2018-03-22 17:24:54 +0900

    [React Native Tutorial] state

--- javascript/react/react-native-sample/App.js
+++ javascript/react/react-native-sample/App.js
@@ -5,41 +5,65 @@ export default class App extends Component {
   render() {
     return (
       <View>
-        <Text>Hello world!</Text>
-        <LotsOfGreetings />
-        <Bananas />
+        <Blink text='I love to blink.' />
+        <Blink text='Yes blinking is so great.' />
+        <Blink text='Why did they ever take this out of HTML.' />
+        <Blink text='Look at me look at me look at me.' />
       </View>
     );
   }
 }
 
-class Bananas extends Component {
-    render() {
-        let pic = {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
-        return (
-            <Image source={pic} style={{width: 193, height: 110}} />
-        );
-    }
-}
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowingText: true,
+    };
 
-class Greeting extends Component {
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
   render() {
+    let display =this.state.isShowingText ? this.props.text: ' ';
     return (
-      <Text>Hello {this.props.name}</Text>
+      <Text>{display}</Text>
     );
   }
 }
 
-class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
-      </View>
-    );
-  }
-}
+// class Bananas extends Component {
+//     render() {
+//         let pic = {
+//             uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+//         };
+//         return (
+//             <Image source={pic} style={{width: 193, height: 110}} />
+//         );
+//     }
+// }
+
+// class Greeting extends Component {
+//   render() {
+//     return (
+//       <Text>Hello {this.props.name}</Text>
+//     );
+//   }
+// }
+
+// class LotsOfGreetings extends Component {
+//   render() {
+//     return (
+//       <View style={{alignItems: 'center'}}>
+//         <Greeting name='Rexxar' />
+//         <Greeting name='Jaina' />
+//         <Greeting name='Valeera' />
+//       </View>
+//     );
+//   }
+// }

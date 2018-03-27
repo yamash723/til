8203a82de9e69ebdd54b506f76b96f2e1d32f727
Shuhei Yamashita <b.e7472@gmail.com>
2018-03-27 17:26:19 +0900

    [React Native Tutorial] handling text input

--- javascript/react/react-native-sample/App.js
+++ javascript/react/react-native-sample/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
-      <View style={{ flex: 1}}>
-        <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
-          <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
-          <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
-        </View>
+      <View style={{ padding: 10 }}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+        
       </View>
     );
   }
--- javascript/react/react-native-sample/README.md
+++ javascript/react/react-native-sample/README.md
@@ -178,3 +178,34 @@ export default class App extends Component {
   }
 }
 ```
+
+### Handling Text Input
+
+* 文字入力ハンドラを持つコンポーネント
+* `onChangeText`で入力内容の変更をハンドリング
+  * 別途`onSubmitEditing`もあり
+
+```javascript
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  render() {
+    return (
+      <View style={{ padding: 10 }}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+        
+      </View>
+    );
+  }
+}
+```

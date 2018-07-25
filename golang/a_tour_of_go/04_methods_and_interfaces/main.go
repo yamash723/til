fc38263caf7c8ebef6520a69126e4a111c08a0c0
Shuhei Yamashita <b.e7472@gmail.com>
2018-07-25 17:53:40 +0900

    [A Tour og Go] Methods and interfaces (14/26)

--- golang/a_tour_of_go/04_methods_and_interfaces/main.go
+++ golang/a_tour_of_go/04_methods_and_interfaces/main.go
@@ -29,6 +29,23 @@ func (v *Vertex) Scale(f float64) {
 	v.Y = v.Y * f
 }
 
+// Interface
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+func (t *T) M() {
+	if t == nil {
+		fmt.Println("<nil>")
+		return
+	}
+	fmt.Println(t.S)
+}
+
 func main() {
 	{
 		// Methods
@@ -43,5 +60,20 @@ func main() {
 		v := Vertex{3, 4}
 		v.Scale(10)
 		fmt.Println(v.Abs())
+	}
+	{
+		// Interface
+		var i I
+		var t *T
+		i = t
+		fmt.Printf("(%v, %T)\n", i, i)
+		i.M()
+
+		var x interface{}
+
+		x = 41
+		fmt.Printf("(%v, %T)\n", x, x)
+		x = "string"
+		fmt.Printf("(%v, %T)\n", x, x)
 	}
 }

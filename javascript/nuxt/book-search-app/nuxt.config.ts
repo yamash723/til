0d747cf2704ad730777d5b1f487890a49270824e
Shuhei Yamashita <b.e7472@gmail.com>
2019-03-14 10:59:47 +0900

    [Nuxt with TypeScript] Nuxt config file change to TypeScript

---
+++ javascript/nuxt/book-search-app/nuxt.config.ts
@@ -0,0 +1,39 @@
+import { Configuration } from "webpack";
+import { Context } from "@nuxt/vue-app";
+
+export default {
+  mode: "spa",
+  head: {
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description" }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  modules: [
+    "@nuxtjs/axios",
+    "@nuxtjs/bulma"
+  ],
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    extend (config: Configuration, ctx: Context): void {
+      if (ctx.isDev && ctx.isClient) {
+        if (config && config.module && config.module.rules) {
+          config.module.rules.push({
+            enforce: "pre",
+            test: /\.(js|vue)$/,
+            loader: "eslint-loader",
+            exclude: /(node_modules)/
+          });
+        }
+      }
+    }
+  }
+};
